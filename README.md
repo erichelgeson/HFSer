52cad9d978d6373f5922be56ae9e326da443ad08
Eric Helgeson <erichelgeson@gmail.com>
2021-09-19 16:36:54 -0500

    Docs

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # HFSer
 
-HFSer is a HFS SCSI Driver tool to extract, swap, and manage SCSI Drivers for vintage Mac 
+HFSer is an HFS SCSI Driver tool to extract, swap, and manage SCSI Drivers for vintage Macintosh computers.
+
+Please make a backup and don't use this on any critical drive image without one.
+
+## Why?
+
+Images for BlueSCSI & RaSCSI can sometimes have a driver that is incompatible with your machine. This allows you to swap out that driver for one that is. 
+
+The primary use would be wanting to swap out Lido to use an image on a Macintosh Plus.
+
+## CLI
+
+An interactive CLI is provided to use most of the libraries functions.
+
+```bash
+./hfser.py <filename>
+```
+
+## Module
+
+This can be imported into other python projects as a module.
+
+## TODO
+
+* Add more drivers
+* Replace `dd` usage with python.
--- hfser/__init__.py
+++ hfser/__init__.py
@@ -8,11 +8,13 @@ drivers_dir = "drivers"
 
 
 def md5_hash(file_name):
+    """Helper to compute and return a hash of a file"""
     import hashlib
     return hashlib.md5(open(file_name, 'rb').read()).hexdigest()
 
 
 def known_drivers():
+    """Return a List of Dicts with the file name and hash of the drivers we know about."""
     driver_files = [f for f in listdir(drivers_dir) if isfile(join(drivers_dir, f))]
     drivers = []
     for file in driver_files:
@@ -22,7 +24,7 @@ def known_drivers():
 
 
 def driver_extract(image, driver_file=None):
-    """Given a file, extract the driver to a temp location and return the path"""
+    """Given a file, extract the driver to a temp location or provided path, and return that path"""
     if driver_file is None:
         driver_file = tempfile.mktemp()
     # TODO: Replace with python bytes instead of call out to dd
@@ -43,7 +45,7 @@ def driver_check(file_name):
 
 
 def switch_driver(file_name, driver):
-    """Given a image file replace the driver with the one provided"""
+    """Given a image file replace the driver with the one provided, and return the new Driver object."""
     # TODO: Replace with python bytes instead of call out to dd
     cmd = ["dd", f"if=drivers/{driver['driver']}", f"of={file_name}", "seek=64", "count=32", "bs=512", "conv=notrunc"]
     proc = subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
